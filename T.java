e0a5299f997383c5cb34e04014196f6d054d2743
17816869682@163.com <a65332794>
2017-07-19 12:04:39 +0800

    文件输入输出例子

---
+++ T.java
@@ -0,0 +1,14 @@
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class T {
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(args));
+		Scanner sc = new Scanner(System.in);
+		System.out.println("重定向读取一个内容:"+sc.next());
+	}
+
+}
